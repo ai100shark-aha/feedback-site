75a2b25740f2ef040610e0e637381f90008a2ab6
상기 <ai100shark@gmail.com>
2026-04-10 16:31:23 +0900

    auto create superuser

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ pip install gunicorn
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; U=get_user_model(); U.objects.filter(username='mr0white').exists() or U.objects.create_superuser('mr0white','','admin1234')" | python manage.py shell
