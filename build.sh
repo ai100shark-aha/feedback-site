d9d182fd929c6d2347800482c39b7ce43e02798b
상기 <ai100shark@gmail.com>
2026-05-27 00:26:51 +0900

    fix: GuideBook 모델 migration 추가, build.sh에 makemigrations 포함

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 pip install gunicorn
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
 python manage.py migrate
 echo "from django.contrib.auth import get_user_model; U=get_user_model(); U.objects.filter(username='mr0white').exists() or U.objects.create_superuser('mr0white','','admin1234')" | python manage.py shell
